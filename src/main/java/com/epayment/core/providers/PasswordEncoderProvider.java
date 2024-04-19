cbf17735bba106068110f37493e2390e8697fe7c
Andre Sch <andresch.dev@gmail.com>
2024-04-19 12:00:43 -0300

    Add password encoder provider

---
+++ src/main/java/com/epayment/core/providers/PasswordEncoderProvider.java
@@ -0,0 +1,12 @@
+package com.epayment.core.providers;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderProvider {
+  public @Bean PasswordEncoder provide() {
+    return new BCryptPasswordEncoder();
+  }
+}
--- src/main/java/com/epayment/core/services/createUser/CreateUserService.java
+++ src/main/java/com/epayment/core/services/createUser/CreateUserService.java
@@ -3,14 +3,21 @@ package com.epayment.core.services.createUser;
 import com.epayment.core.entities.User;
 import com.epayment.core.repositories.UserRepository;
 import com.epayment.core.exceptions.DuplicateUserException;
+
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class CreateUserService {
   private UserRepository userRepository;
+  private PasswordEncoder passwordEncoder;
 
-  public CreateUserService(UserRepository repository) {
-    this.userRepository = repository;
+  public CreateUserService(
+    UserRepository userRepository,
+    PasswordEncoder passwordEncoder
+  ) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   public User execute(UserCreationRequest request) {
@@ -26,7 +33,7 @@ public class CreateUserService {
     user.setFullName(request.fullName);
     user.setEmail(request.email);
     
-    String passwordHash = Integer.toString(request.password.hashCode()); // stub
+    String passwordHash = this.passwordEncoder.encode(request.password);
     user.setPassword(passwordHash);
 
     this.userRepository.save(user);
