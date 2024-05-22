6516565d8d00d2d0d053d39096a3b26f3a1a69f3
Andre Sch <andresch.dev@gmail.com>
2024-05-22 17:07:34 -0300

    Remove unsafe deposit

--- src/main/java/com/epayment/core/interfaces/CashMachine.java
+++
@@ -1,8 +0,0 @@
-package com.epayment.core.interfaces;
-
-import java.math.BigDecimal;
-
-public interface CashMachine {
-  public void withdraw(BigDecimal amount);
-  public BigDecimal readDepositedAmount();
-}
