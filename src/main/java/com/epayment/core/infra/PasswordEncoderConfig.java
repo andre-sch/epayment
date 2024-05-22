020226dd70745c2f1447a4a280d34fc1c93b2103
Andre Sch <andresch.dev@gmail.com>
2024-05-22 17:09:11 -0300

    Move spring configuration to infra/

--- src/main/java/com/epayment/core/infra/PasswordEncoderConfig.java
+++ src/main/java/com/epayment/core/infra/PasswordEncoderConfig.java
@@ -1,11 +1,11 @@
-package com.epayment.core.providers;
+package com.epayment.core.infra;
 
 import org.springframework.context.annotation.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class PasswordEncoderProvider {
+public class PasswordEncoderConfig {
   public @Bean PasswordEncoder provideBCryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
   }
