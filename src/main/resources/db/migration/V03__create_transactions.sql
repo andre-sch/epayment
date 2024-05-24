fc150c549bc4b0efb79c8e7dbe3a0f5784f25e9f
Andre Sch <andresch.dev@gmail.com>
2024-05-24 16:31:35 -0300

    Fix transaction foreign key reference

--- src/main/resources/db/migration/V03__create_transactions.sql
+++ src/main/resources/db/migration/V03__create_transactions.sql
@@ -4,6 +4,6 @@ create table if not exists transactions (
   receiver_id integer not null,
   amount decimal(10, 2) not null,
   executed_at timestamp default current_timestamp,
-  foreign key (sender_id) references users (id),
-  foreign key (receiver_id) references users (id)
+  foreign key (sender_id) references wallets (id),
+  foreign key (receiver_id) references wallets (id)
 );
