110ad8443ae3c7e62dd95f133a9fa3d5c7069088
Andre Sch <andresch.dev@gmail.com>
2024-05-31 19:07:58 -0300

    Mock transaction event dispatcher

--- src/test/java/com/epayment/core/integration/TransferResourceServiceTests.java
+++ src/test/java/com/epayment/core/integration/TransferResourceServiceTests.java
@@ -8,17 +8,24 @@ import com.epayment.core.utils.DummyWalletFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class TransferResourceServiceTests {
   private final BigDecimal zero = BigDecimal.valueOf(0L, 2);
   private final BigDecimal amount = BigDecimal.valueOf(1L, 2);
   private final DummyWalletFactory walletFactory = new DummyWalletFactory();
+  private final TransactionParser transactionParser = new TransactionParser();
 
+  @MockBean private EventDispatcher<BalanceChanged> eventDispatcher;
   @Autowired private UserRepository userRepository;
   @Autowired private WalletRepository walletRepository;
   @Autowired private TransactionRepository transactionRepository;
@@ -42,6 +49,10 @@ public class TransferResourceServiceTests {
     assertThat(transaction.getAmount()).isEqualTo(amount);
     assertThat(transaction.getReceiver().getBalance()).isEqualTo(amount.add(amount));
     assertThat(transaction.getSender().getBalance()).isEqualTo(zero);
+    transactionParser
+      .parseBalanceChangesOf(transaction)
+      .forEach((var change) -> verify(eventDispatcher).dispatch(change));
+    
     this.transactionRepository.deleteAll();
   }
 
