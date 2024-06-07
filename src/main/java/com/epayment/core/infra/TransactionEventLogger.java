9612fff21ddcb0604f3ada060cb66106c110999b
Andre Sch <andresch.dev@gmail.com>
2024-06-07 15:30:23 -0300

    Replace native logging with slf4j

--- src/main/java/com/epayment/core/infra/TransactionEventLogger.java
+++ src/main/java/com/epayment/core/infra/TransactionEventLogger.java
@@ -2,11 +2,13 @@ package com.epayment.core.infra;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class TransactionEventLogger {
   @KafkaListener(topics = {"balances"}, groupId = "logger")
   public void log(String event) {
-    System.out.println(event);
+    log.info(event);
   }
 }
