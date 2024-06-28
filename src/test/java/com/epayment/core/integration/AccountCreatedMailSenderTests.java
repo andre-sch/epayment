911c355ac8d7e3598c0d814f4ce6f0c21d090176
Andre Sch <andresch.dev@gmail.com>
2024-06-28 19:46:14 -0300

    Add tests for AccountCreatedMailSender

---
+++ src/test/java/com/epayment/core/integration/AccountCreatedMailSenderTests.java
@@ -0,0 +1,62 @@
+package com.epayment.core.integration;
+
+import java.math.BigDecimal;
+import com.epayment.core.domain.AccountCreated;
+import com.epayment.core.application.interfaces.JsonConverter;
+import com.epayment.core.adapters.kafka.AccountCreatedMailSender;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.*;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountCreatedMailSenderTests {
+  @Autowired private JavaMailSender javaMailSender;
+  @Autowired private AccountCreatedMailSender eventMailSender;
+  @Autowired private JsonConverter json; 
+
+  @Test
+  public void mailSending() {
+    // arrange
+    String serializedEvent = json.serialize(
+      new AccountCreated(
+        BigDecimal.valueOf(1001L, 2),
+        "client@email",
+        "clientName"
+      )
+    );
+
+    // act
+    eventMailSender.send(serializedEvent);
+
+    // assert
+    var expectedMessage = new SimpleMailMessage();
+
+    expectedMessage.setTo("client@email");
+    expectedMessage.setSubject("Your Epayment account has been created successfully");
+    expectedMessage.setText(
+      """
+      Dear clientName,
+
+      Your account creation has been successfully processed.
+      Below are the details of your account:
+
+      Balance: 10.01$
+      Full name: clientName
+      Email: client@email
+
+      Thank you for trust in our services.
+
+      Best regards,
+      Epayment
+      """
+    );
+    
+    verify(javaMailSender).send(expectedMessage);
+  }
+}
